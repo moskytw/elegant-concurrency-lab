9955f670e3c8507f4e5ab0cb2d29c506d4c7daad
Mosky <mosky.tw@gmail.com>
2017-06-08 13:00:25 +0800

    Also benchmark queue

--- benchmark_locks.py
+++ benchmark_locks.py
@@ -4,11 +4,14 @@
 
 from timeit import timeit
 from threading import Lock, RLock
+from queue import Queue
 
 
 x = 0
 lock = Lock()
 rlock = RLock()
+queue = Queue()
+queue.put(1)  # = the get won't block = released lock
 
 
 def run_with_nothing():
@@ -33,21 +36,33 @@ def run_with_rlock():
     rlock.release()
 
 
+def run_with_queue():
+    queue.get()  # let other gets block
+    x+1
+    queue.put(1)  # let one get continue
+
+
 if __name__ == '__main__':
 
-    ctrl = expt = timeit(run_with_abs)
-    print(f'Run with abs      {expt:<06.4}s  {expt/ctrl:<06.4}x')
+    N = 1000000  # the default value
 
-    expt = timeit(run_with_nothing)
-    print(f'Run with nothing  {expt:<06.4}s  {expt/ctrl:<06.4}x')
+    ctrl = expt = timeit(run_with_abs, number=N)
+    print(f'Run with abs      {expt:6.4f}s  {expt/ctrl:7.4f}x')
 
-    expt = timeit(run_with_lock)
-    print(f'Run with lock     {expt:<06.4}s  {expt/ctrl:<06.4}x')
+    expt = timeit(run_with_nothing, number=N)
+    print(f'Run with nothing  {expt:6.4f}s  {expt/ctrl:7.4f}x')
 
-    expt = timeit(run_with_rlock)
-    print(f'Run with rlock    {expt:<06.4}s  {expt/ctrl:<06.4}x')
+    expt = timeit(run_with_lock, number=N)
+    print(f'Run with lock     {expt:6.4f}s  {expt/ctrl:7.4f}x')
 
-    # Run with abs      0.4113s  1.0000x
-    # Run with nothing  0.2348s  0.5709x
-    # Run with lock     0.6629s  1.6110x
-    # Run with rlock    0.6630s  1.6120x
+    expt = timeit(run_with_rlock, number=N)
+    print(f'Run with rlock    {expt:6.4f}s  {expt/ctrl:7.4f}x')
+
+    expt = timeit(run_with_queue, number=N//10)*10
+    print(f'Run with queue    {expt:6.4f}s  {expt/ctrl:7.4f}x')
+
+    # Run with abs      0.3458s   1.0000x
+    # Run with nothing  0.1982s   0.5732x
+    # Run with lock     0.6050s   1.7495x
+    # Run with rlock    0.6192s   1.7907x
+    # Run with queue    7.4945s  21.6723x
