6bde636b48c9dbc37a16b0de6f71556aca06afb2
Mosky <mosky.tw@gmail.com>
2017-06-08 12:50:16 +0800

    Commit consume_urls_with_*.py

---
+++ consume_urls_with_channel.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from threading import Thread
+from queue import Queue
+
+import requests
+
+
+# make a distinguishing and debuggable object
+TO_RETURN = ['to_return']
+
+
+def consume_url_q(url_q):
+
+    while True:
+
+        url = url_q.get()
+
+        if url is TO_RETURN:
+            return
+
+        content = requests.get(url).content
+        print('Queried', len(content), 'bytes from', url)
+
+
+def start_in_thread(f, *args, **kwargs):
+    t = Thread(target=f, args=args, kwargs=kwargs)
+    t.start()
+    return t
+
+
+if __name__ == '__main__':
+
+    N = 2
+
+    urls = [
+        'https://tw.pycon.org/',
+        'https://tw.pycon.org/2017/en-us/events/keynotes/',
+        'https://tw.pycon.org/2017/en-us/events/schedule/',
+        'https://tw.pycon.org/2017/en-us/venue/',
+    ]
+
+    url_q = Queue()
+    for url in urls:
+        url_q.put(url)
+
+    for _ in range(N):
+        url_q.put(TO_RETURN)
+
+    for _ in range(N):
+        start_in_thread(consume_url_q, url_q)
---
+++ consume_urls_with_task_done.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from threading import Thread
+from queue import Queue
+
+import requests
+
+
+def consume_url_q(url_q):
+
+    while True:
+
+        url = url_q.get()
+
+        content = requests.get(url).content
+        print('Queried', len(content), 'bytes from', url)
+
+        # mark a task is done
+        url_q.task_done()
+
+
+def start_in_daemon_thread(f, *args, **kwargs):
+    t = Thread(target=f, daemon=True, args=args, kwargs=kwargs)
+    t.start()
+    return t
+
+
+if __name__ == '__main__':
+
+    urls = [
+        'https://tw.pycon.org/',
+        'https://tw.pycon.org/2017/en-us/events/keynotes/',
+        'https://tw.pycon.org/2017/en-us/events/schedule/',
+        'https://tw.pycon.org/2017/en-us/venue/',
+    ]
+
+    url_q = Queue()
+    for url in urls:
+        url_q.put(url)
+
+    for _ in range(2):
+        # the “daemon” is not the Unix's deamon
+        # daemon threads are abruptly stopped at shutdown
+        start_in_daemon_thread(consume_url_q, url_q)
+
+    # block and unblock when all tasks are done
+    url_q.join()
+
+    # when main thread exits, the interpreter shuts down
